cdec12e82547e2b3b2addb33e9ebe44106d68b2d
by.huang <chengshi.huang@cootek.cn>
2017-11-15 02:47:04 +0800

    商城列表优化

--- gogo/page/GoodsCell.m
+++ gogo/page/GoodsCell.m
@@ -11,7 +11,9 @@
 @interface GoodsCell()
 
 @property (strong, nonatomic) UIImageView *showImg;
-
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *coinLabel;
+@property (strong, nonatomic) UILabel *countLabel;
 
 @end
 
@@ -33,10 +35,41 @@
     _showImg.contentMode = UIViewContentModeScaleAspectFill;
     [self.contentView addSubview:_showImg];
     
+    UIView *view = [[UIView alloc]init];
+    view.alpha = 0.75;
+    view.backgroundColor = [UIColor blackColor];
+    view.frame = CGRectMake(0, [PUtil getActualHeight:230], self.bounds.size.width, self.bounds.size.height - [PUtil getActualHeight:230]);
+    [self.contentView addSubview:view];
+    
+    _titleLabel = [[UILabel alloc]init];
+    _titleLabel.frame =  CGRectMake([PUtil getActualWidth:15], [PUtil getActualHeight:15], self.bounds.size.width -[PUtil getActualWidth:30] *2, [PUtil getActualHeight:30]);
+    _titleLabel.textColor = c08_text;
+    _titleLabel.font = [UIFont systemFontOfSize:[PUtil getActualHeight:28]];
+    [view addSubview:_titleLabel];
+    
+    _coinLabel = [[UILabel alloc]init];
+    _coinLabel.frame =  CGRectMake([PUtil getActualWidth:15], [PUtil getActualHeight:55], self.bounds.size.width -[PUtil getActualWidth:30] , [PUtil getActualHeight:30]);
+    _coinLabel.textColor = c03_yellow;
+    _coinLabel.font = [UIFont systemFontOfSize:[PUtil getActualHeight:24]];
+    [view addSubview:_coinLabel];
+
+    
+    _countLabel = [[UILabel alloc]init];
+    _countLabel.textColor = c08_text;
+    _countLabel.alpha = 0.5f;
+    _countLabel.font = [UIFont systemFontOfSize:[PUtil getActualHeight:24]];
+    [view addSubview:_countLabel];
+
+    
 }
 
 -(void)setData : (GoodsModel *)goodsModel{
     [_showImg sd_setImageWithURL:[NSURL URLWithString:goodsModel.cover]];
+    _titleLabel.text  = goodsModel.title;
+    _coinLabel.text = [NSString stringWithFormat:@"%ld竞猜币",goodsModel.coin];
+    _countLabel.text = [NSString stringWithFormat:@"剩余%ld件",goodsModel.total];
+    
+    _countLabel.frame =  CGRectMake(self.bounds.size.width - [PUtil getActualWidth:15] - _countLabel.contentSize.width, [PUtil getActualHeight:55], _countLabel.contentSize.width , _countLabel.contentSize.height);
 
 }
 
